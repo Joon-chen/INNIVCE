0976ca5cf84caeb7833a81ef1e0ed1bc07a71722
Jun Chen <jun@192.168.2.23>
2026-06-30 01:11:23 +0800

    fix(runtime): route company understanding questions

--- tests/test_runtime_v5.py
+++ tests/test_runtime_v5.py
@@ -209,6 +209,7 @@ def test_runtime_v5_company_questions_route_to_general_knowledge_query(monkeypat
         "能躬行科技公司是做什么的，你知道吗",
         "公司的客户有哪些",
         "公司有哪些产品",
+        "公司的优势是什么",
         "公司的联系方式是什么",
     ):
         intent = recognize_intent(question, _context(question))
@@ -4272,6 +4273,7 @@ def test_command_plan_domain_gate_records_reason_source_and_confidence(monkeypat
         ("公司是做什么的", "Knowledge", "foundation_route:knowledge.company_profile", "foundation_rule"),
         ("公司有哪些产品", "Knowledge", "foundation_route:knowledge.company_profile", "foundation_rule"),
         ("公司的客户有哪些", "Knowledge", "foundation_route:knowledge.company_profile", "foundation_rule"),
+        ("公司的优势是什么", "Knowledge", "foundation_route:knowledge.company_profile", "foundation_rule"),
         ("公司的联系方式是什么", "Knowledge", "foundation_route:knowledge.company_profile", "foundation_rule"),
         ("报销流程怎么做", "Knowledge", "foundation_route:knowledge.general", "foundation_rule"),
         ("我有多少封邮件", "Communication", "foundation_route:communication.mail", "foundation_rule"),
